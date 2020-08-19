0667e464d9235a1b47186d960775ed4228f2ab65
Nguyen Van Ha <hanguyen11mm@gmail.com>
2020-08-19 23:32:58 +0700

    add account entity

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import com.example.demo.entity.account.Account;
+import com.example.demo.entity.account.AccountDto;
+import com.example.demo.utils.AssignUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +11,15 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+        Account account = new Account();
+        account.setId(1);
+        account.setUsername("ha11");
+        AccountDto accountDto = new AccountDto();
+        try {
+            AssignUtils.assign(accountDto, account);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 
 }
